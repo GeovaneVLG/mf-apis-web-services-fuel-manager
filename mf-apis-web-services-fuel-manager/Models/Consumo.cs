d45d6dac69187aaebc5d5d8f0b6e6e64b292fa1d
GeovaneVLG <geovanevinicius2012@hotmail.com>
2023-04-07 13:36:34 -0300

    Classes Consumo e Veiculo

---
+++ mf-apis-web-services-fuel-manager/Models/Consumo.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mf_apis_web_services_fuel_manager.Models
+{
+    public class Consumo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Descricao { get; set; }
+
+        public DateTime Data { get; set; }
+
+        public decimal Valor { get; set; }
+
+        public TipoCombustivel Tipo { get; set; }
+
+    }
+
+    public enum TipoCombustivel{
+        Diesel,
+        Etanol,
+        Gasolina
+    }
+}
