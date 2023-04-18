4ce2e1bce85461831cabc37634965f4eaef13bdf
GeovaneVLG <geovanevinicius2012@hotmail.com>
2023-04-18 11:35:50 -0300

    criação Controller Usuario, criptografia da senha

--- mf-apis-web-services-fuel-manager/Controllers/VeiculosController.cs
+++ mf-apis-web-services-fuel-manager/Controllers/VeiculosController.cs
@@ -31,12 +31,15 @@ namespace mf_apis_web_services_fuel_manager.Controllers
             _context.Veiculos.Add(model);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetById", new {id= model.Id}, model);
+            return CreatedAtAction("GetById", new { id = model.Id }, model);
         }
-        [HttpGet ("{id}")]
+        [HttpGet("{id}")]
         public async Task<ActionResult> GetById(int id)
         {
-            var model = await _context.Veiculos.Include(t => t.Consumos).FirstOrDefaultAsync(c => c.Id == id);
+            var model = await _context.Veiculos
+                .Include(t => t.Usuarios).ThenInclude(t => t.Usuario)
+                .Include(t => t.Consumos)
+                .FirstOrDefaultAsync(c => c.Id == id);
 
             if (model == null) return NotFound();
 
@@ -72,9 +75,33 @@ namespace mf_apis_web_services_fuel_manager.Controllers
 
         private void GerarLinks(Veiculo model)
         {
-            model.Links.Add(new LinkDto(model.Id, Url.ActionLink() , rel: "self", metodo: "GET"));
+            model.Links.Add(new LinkDto(model.Id, Url.ActionLink(), rel: "self", metodo: "GET"));
             model.Links.Add(new LinkDto(model.Id, Url.ActionLink(), rel: "update", metodo: "PUT"));
             model.Links.Add(new LinkDto(model.Id, Url.ActionLink(), rel: "delete", metodo: "DELETE"));
+        }
+        [HttpPost("{id}/usuarios")]
+        public async Task<ActionResult> AddUsuario(int id, VeiculoUsuarios model)
+        {
+            if(id != model.VeiculoId) return BadRequest();
+            _context.VeiculosUsuarios.Add(model);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetById", new { id = model.VeiculoId }, model);
+        }
+        [HttpDelete("{id}/usuarios/{usuarioId}")]
+        public async Task<ActionResult> DeleteUsuario(int id, int usuarioId)
+        {
+            var model = await _context.VeiculosUsuarios
+                .Where(c => c.VeiculoId == id && c.UsuarioId == usuarioId)
+                .FirstOrDefaultAsync();
+
+            if(model == null) return NotFound();
+
+            _context.VeiculosUsuarios.Remove(model);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+
         }
     }
 }
